884094f3991059d457974c7eef54c24d5d5afb7e
Faraz Fatahnaie <88314400+Faraz-Fatahnaie@users.noreply.github.com>
2023-03-17 12:20:01 +0330

    Delete temp.py

--- face_module/temp.py
+++
@@ -1,7 +0,0 @@
-import torch
-from settings import DIR
-from pathlib import Path
-import sys
-#sys.path.insert(0, './trained_model')
-
-FAS = torch.load(Path(DIR.joinpath('face_module/trained_model/model_checkpoint/FAS.pt')), map_location=torch.device('cpu'))
